f4a791409b65eb9f2d409fbbfb1f98d8b5d6cf8c
Matic Bozic <bozicmatic.bm@gmail.com>
2021-01-20 17:00:48 +0100

    Added the city component that displays city weather information

---
+++ src/components/City.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="info">
+      <div class="city-info">
+         {{ city }} <br>
+         {{ description }}
+      </div>
+      <div class="weather-info">
+          {{ temperature }}
+          {{ humidity }}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'City',
+    props: {
+        city: {
+            type: String,
+            required: true
+        },
+        temperature: {
+            type: Number,
+            required: true
+        },
+        humidity: {
+            type: Number,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+    .info {
+        width: 100%;
+    }
+    .city-info {
+        float: left;
+    }
+    .weather-info {
+        float: right;
+    }
+</style>
