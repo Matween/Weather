c1c977eaa9b8139ce59fe8e78106c8e9e3dfadaf
Matic Bozic <bozicmatic.bm@gmail.com>
2021-01-20 15:14:25 +0100

    Added component for adding cities

---
+++ src/components/AddCity.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <form>
+          <input type="text" placeholder="City name" v-model="city">
+          <input type="button" value="Cancel">
+          <input type="submit" value="Finish">
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AddCity',
+    data() {
+        return {
+            city
+        }
+    },
+    methods() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
