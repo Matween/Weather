f58b118ecbb638ccfbc1b0f09fa6f1709ebc932e
Matic Bozic <bozicmatic.bm@gmail.com>
2021-01-20 15:01:40 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CityList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CityList from './components/CityList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CityList
   }
 }
 </script>
---
+++ src/components/CityList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul class="cities" v-if="cities.length > 0"></ul>
+
+    <div v-if="cities.length === 0">No cities.</div>
+
+    <button>Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CityList",
+  data() {
+      return {
+        cities: [],
+      }
+  },
+};
+</script>
+
+<style>
+</style>
